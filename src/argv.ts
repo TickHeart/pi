fe76878d8ceae46f1ec9a1dd3c16fc9550342db8
tickheart <66043405+TickHeart@users.noreply.github.com>
2022-07-15 16:55:50 +0800

    修改了指定包管理器验证未通过的bug

--- src/argv.ts
+++ src/argv.ts
@@ -22,17 +22,17 @@ function detectionInstruction(argv: string[]) {
   const npmAlias = cmds.npm.split('|')
 
   for (const p of pnpmAlias) {
-    if (param[p])
+    if (param[p.replace(/-*/g, '')])
       return 'pnpm'
   }
 
   for (const y of yarnAlias) {
-    if (param[y])
+    if (param[y.replace(/-*/g, '')])
       return 'yarn'
   }
 
   for (const n of npmAlias) {
-    if (param[n])
+    if (param[n.replace(/-*/g, '')])
       return 'npm'
   }
 
