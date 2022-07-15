28c5ecb925e53b3aa35ab9ee7d225143bd442d8f
zhaoyuanmeng <1426593075@qq.com>
2022-07-15 17:10:30 +0800

    test: 💍 aaa
    asda
    
    BREAKING CHANGE: 🧨 asdasd

--- commitlint.config.js
+++ commitlint.config.js
@@ -27,17 +27,17 @@ module.exports = {
     // <type> 不能为空
     'type-empty': [2, 'never'],
     // <scope> 范围不能为空
-    'scope-empty': [2, 'never'],
+    // 'scope-empty': [2, 'never'],
     // <scope> 范围格式
-    'scope-case': [0],
+    // "scope-case": [0],
     // <subject> 主要 message 不能为空
     'subject-empty': [2, 'never'],
     // <subject> 以什么为结束标志，禁用
-    'subject-full-stop': [0, 'never'],
+    // "subject-full-stop": [0, "never"],
     // <subject> 格式，禁用
-    'subject-case': [0, 'never'],
+    // "subject-case": [0, "never"],
     // <body> 以空行开头
-    'body-leading-blank': [1, 'always'],
-    'header-max-length': [0, 'always', 72],
+    // 'body-leading-blank': [1, 'always'],
+    // 'header-max-length': [0, 'always', 72],
   },
 }
